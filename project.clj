fa26aff2d98850bcb5543b0175227be299aca527
Dave Roberts <dave@droberts.com>
2022-01-16 14:35:05 -0600

    Add standard Leiningen project structure to repository.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojurecrazy "0.1.0-SNAPSHOT"
+  :description "Clojure Crazy - A Blog and Sample Code"
+  :url "https://github.com/dgr/clojurecrazy"
+  :license {:name "multiple"
+            :comments "The materials in this project are covered under multiple licenses. See the LICENSE file for more specific information."}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [criterium "0.4.6"]]
+  :repl-options {:init-ns user})
